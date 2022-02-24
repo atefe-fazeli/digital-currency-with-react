c47504e3d78e28743248a37f0d1c6c110657b8ab
atefe-fazeli <atefefazeli1996@gmail.com>
2022-02-24 13:03:29 +0330

    search completed

--- src/components/Curency.js
+++ src/components/Curency.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import styles from "./Curency.module.css";
 
 const Curency = ({image,name,symbol,currentprice,marketcap,marketchange}) => {
     return (
-        <div>
-            <img src={image} alt="loading" />
+        <div className={styles.container}>
+            <img  src={image} alt="loading" />
                 <span>{name}</span>
-                <span>{symbol}</span>
-                <span>{currentprice}</span>
-                <span>{marketcap}</span>
+                <span>{symbol.toUpperCase()}</span>
+                <span>{currentprice.toLocaleString()}</span>
+                <span>{marketcap.toLocaleString()}</span>
                 <span>{marketchange}</span>
           
             
--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { GetCoin } from '../services/Getcoin';
 import Curency from './Curency';
+import Loader from './Loader';
 
 const Landing = () => {
     const [coins,setCoins]=useState([]);
@@ -9,31 +10,41 @@ const Landing = () => {
       const fetchAPI=async()=>{
           const data =await GetCoin();
           setCoins(data);
+         
       }
       fetchAPI();
    }   
    ,[]) 
-     const[search,setSearch]=useState("")     
-     chengeHandler=(event)=>{
+     const[search,setSearch]=useState("");     
+    const changeHandler=(event)=>{
      setSearch(event.target.value)
      console.log(search)
-     }     
+    
+     }  
+     const trueSearch=coins.filter(coin=>coin.name.toLowerCase().includes(search.toLowerCase()))   
   
     return (
-        <div>
-         <input type="text"  placeholder="search..." onChenge={chengeHandler}></input> 
-         <div>
-          { coins.map((coin) =><Curency 
-          key={coin.id} 
-          name={coin.name}
-          symbol={coin.symbol} 
-          currentprice={coin.current_price} 
-          image={coin.image}
-          marketcap={coin.market_cap} 
-          marketchange={coin.market_cap_change_24h}
-          />)} 
-          </div>
-        </div>
+       <>
+         <input type="text"   placeholder="search..." onChange={changeHandler}></input> 
+        
+          { coins.length  ?
+          <div>
+            {
+              trueSearch.map((coin) =><Curency 
+              key={coin.id} 
+              name={coin.name}
+              symbol={coin.symbol} 
+              currentprice={coin.current_price} 
+              image={coin.image}
+              marketcap={coin.market_cap} 
+              marketchange={coin.market_cap_change_24h}
+            />)
+          }
+            </div>
+        : <Loader />
+      } 
+          
+       </>
     );
 };
 
