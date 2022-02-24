8cd010cd2250bf0b6ffd28392530ac6b25c7d240
atefe-fazeli <atefefazeli1996@gmail.com>
2022-02-24 19:01:24 +0330

    lottie added to Loader component

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import Lottie from 'react-lottie';
+import lottie from "react-lottie";
+import loadingData from "../loading-car.json"
 
 const Loader = () => {
+    const defaultOptions = {
+        loop: true,
+        autoplay: true,
+        animationData: loadingData,
+      };
     return (
         <div>
-           loading
+           <Lottie
+           options={defaultOptions}
+           width={400}
+           height={400}
+           />
         </div>
     );
 };
